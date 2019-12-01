74d5ccf3bde6130c72776d4d89ac106447ca107e
gabbmatias <gabrielmatiaas97@gmail.com>
2019-12-01 00:17:11 -0400

    Criacao das classes Bo

---
+++ src/model/bo/EmployeeBo.java
@@ -0,0 +1,19 @@
+package model.bo;
+
+public class EmployeeBo {
+    public void addEmployee(){
+
+    }
+
+    public void alterEmployee(){
+
+    }
+
+    public void removeEmployee(){
+
+    }
+
+    public void  login(){
+
+    }
+}
